aeb86e69a619a1b2427cb0e6f2c83cf2cca1b8a6
Теодор Розберг <C:\Users\teddy\AppData\Roaming\The Bat!>
2026-01-20 18:50:01 +0300

    style: increase logo size

--- components/tracker/BrandHeader.tsx
+++ components/tracker/BrandHeader.tsx
@@ -3,12 +3,12 @@ import Image from "next/image";
 export function BrandHeader() {
   return (
     <div className="flex w-full items-center justify-start px-2">
-      <div className="flex h-16 w-16 items-center justify-center overflow-hidden rounded-3xl bg-white shadow-brand">
+      <div className="flex h-20 w-20 items-center justify-center overflow-hidden rounded-[32px] bg-white shadow-brand">
         <Image
           src="/logo.png"
           alt="RTE-Consult"
-          width={56}
-          height={56}
+          width={72}
+          height={72}
         />
       </div>
     </div>
